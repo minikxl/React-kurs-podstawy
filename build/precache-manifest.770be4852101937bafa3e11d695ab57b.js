9dc9570691d1757ad2e1138c52533d8526b7d8be
Dominik Kras <xlminikxl@gmail.com>
2020-08-23 14:55:17 +0200

    commit

---
+++ build/precache-manifest.770be4852101937bafa3e11d695ab57b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd51ccd9cbd093c091bb9bbf472a7197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce06d3c10705c0cb8de",
+    "url": "/static/css/main.386c4283.chunk.css"
+  },
+  {
+    "revision": "8e74f6213595585fee2f",
+    "url": "/static/js/2.0d62ccfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0d62ccfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce06d3c10705c0cb8de",
+    "url": "/static/js/main.1a01599f.chunk.js"
+  },
+  {
+    "revision": "51527d7e6f9a847aebb0",
+    "url": "/static/js/runtime-main.585204ed.js"
+  },
+  {
+    "revision": "a7efe9393f85a8757970682cf58a98c3",
+    "url": "/static/media/logo.a7efe939.svg"
+  }
+]);
